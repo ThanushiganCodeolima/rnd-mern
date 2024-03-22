1d25dff6a73f75abc1488d88e4d4c0e146752b21
Thanushigan Codeolima <163111964+ThanushiganCodeolima@users.noreply.github.com>
2024-03-22 15:27:56 +0530

    Verson Prons

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import Content from './Components/Content'
+import Footer from './Components/Footer';
+import Header from './Components/Header'
 function App() {
+  const appName = `Codeolima`
+  const verion = `1.0.0`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header appName={appName}/>
+    <Content />
+    <Footer verion={verion}/>
+    </>
   );
 }
 
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer(verion) {
+  return (
+    <div>{verion}</div>
+  )
+}
+
+export default Footer
