17b6f98578860bb0967937c3fe64476acca16ab0
Thanushigan Codeolima <163111964+ThanushiganCodeolima@users.noreply.github.com>
2024-03-26 15:52:10 +0530

    "useState"

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Header() {
-  return (
-    <div>Header</div>
-  )
+export default class Header extends Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>{this.props.text}</div>
+    )
+  }
 }
-
-export default Header
