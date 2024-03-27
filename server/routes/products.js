9ef49b3973cc01e86ad37e29ac052be2a031563b
Thanushigan Codeolima <163111964+ThanushiganCodeolima@users.noreply.github.com>
2024-03-27 15:08:51 +0530

    Add Product Get By Id Route

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,15 +4,29 @@ var router = express.Router('express');
 const { DUMMY_PRODUCT_LIST } = require('../dummy/dummy-products');
 var router = express.Router();
 
-/*List Products*/
+// List Products 
 router.get('/', function(req, res, next) {
 
-    return res.status(200).json(DUMMY_PRODUCT_LIST) 
+    return res.status(200).json(DUMMY_PRODUCT_LIST)
+    const products = DUMMY_PRODUCT_LIST
+    return res.status(200).json(products)
+
 });
 
-// TODO :
-// Product Get By Id 
 
+// Product Get By Id 
+router.get('/:id', function(req, res, next) {
+
+    const id = req.params.id;
+    const product = DUMMY_PRODUCT_LIST.find((item) => item._id === id)
+    if(product){
+        return res.status(200).json(product)
+    }else{
+        return res.status(404).json()
+    }
+
+});
+// TODO :
 // Create Product
 
 // Product Update by Id 
