29f74c2a38a1fd33829f94fe8948952b22a58069
Thanushigan Codeolima <163111964+ThanushiganCodeolima@users.noreply.github.com>
2024-04-01 10:46:51 +0530

    LifeCycleA for mounting

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Content from './Components/Content'
 import Footer from './Components/Footer';
 import Header from './Components/Header'
+import LifecycleA from './Components/LifecycleA';
 function App() {
   const appName = `Codeolima Web App`
   const verion = process.env.VERSION;
@@ -10,6 +11,7 @@ function App() {
     <Header appName={appName}/>
     <Content />
     <Footer verion={verion}/>
+    <LifecycleA />
     </>
   );
 }
---
+++ client/src/Components/LifecycleA.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class LifecycleA extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         name: 'CodeOlima'
+      }
+      console.log('LifecycleA constructor')
+    }
+    static getDerivedStateFromProps(props,state){
+        console.log('LifecycleA getDerivedStateFromProps')
+        return null
+    }
+    render(){
+        console.log('LifecycleA render')
+        return <div>LifecycleA</div>
+        
+    }
+    componentDidMount(){
+        console.log('LifecycleA componentDidMount')
+    }
+    render() {
+    return <div>LifecycleA</div>
+  }
+}
+
+export default LifecycleA
