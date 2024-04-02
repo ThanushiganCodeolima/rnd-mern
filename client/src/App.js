dace056149d239eb61c7a90902113eca5cc4f40d
Thanushigan Codeolima <163111964+ThanushiganCodeolima@users.noreply.github.com>
2024-04-02 09:01:10 +0530

    add click events some css and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
-import './App.css'
+import './index.css'
 import Content from './Components/Content'
 import Footer from './Components/Footer';
 import Header from './Components/Header'
 import LifecycleA from './Components/LifecycleA';
+import Navbar from './Components/Navbar';
 function App() {
   const appName = `Codeolima Web App`
   const verion = process.env.VERSION;
   return (
-    <>
-    <Header appName={appName}/>
-    <Content />
-    <Footer verion={verion}/>
-    <LifecycleA />
-    </>
+    <div className='App'>
+      <Navbar />
+      <div className='content'>
+        <Content />
+      </div>
+      {/*<Header appName={appName}/>
+      <Footer verion={verion}/>
+      <LifecycleA />*/}
+    </div>
   );
 }
 
--- client/src/Components/Content.js
+++ client/src/Components/Content.js
@@ -1,8 +1,17 @@
-import React from 'react'
+const Content = () => {
 
-function Content() {
+  const handleClick = (e) => {
+    console.log('Hello Guys', e)
+  }
+  const handleClickAgain = (name, e) =>{
+    console.log('hello' + name, e.target)
+  }
   return (
-    <div>Content</div>
+    <div className='home'>
+      <h2>Content</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain('thanush')}>Click me again</button>
+    </div>
   )
 }
 
