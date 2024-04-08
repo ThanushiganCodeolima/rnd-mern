41e06a91daf3b53995c1e8acf8d4c8edce103da0
Thanushigan Codeolima <163111964+ThanushiganCodeolima@users.noreply.github.com>
2024-04-08 13:44:51 +0530

    add update by id

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Product = require('../models/products')
+var { validateRequestPayload } = require('../util/validateRequestPayload')
 
 // List Products  
 router.get('/', async (req, res, next) => {
@@ -33,50 +34,20 @@ router.get('/:id', async (req, res, next) => {
 
 });
 
-// Create Product
-router.put('/:id', async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const { name, description, price, timestamp } = req.body;
-        console.log(name, description, price, timestamp);
-
-        if (!name) {
-            return res.status(400).json("Name is not defined");
-        }
-
-        if (!description) {
-            return res.status(400).json("Description is not defined");
-        }
-
-        if (!price || isNaN(price)) {
-            return res.status(400).json("Price is not defined or is not a number");
-        }
-
-        if (!timestamp) {
-            return res.status(400).json("Timestamp is not defined");
-        }
-
-        const product = await Product.updateOne({ _id: id }, { name: name, description: description, price: price, timestamp: timestamp });
-        
-        if (product) {
-            return res.status(200).json(product);
-        } else {
-            return res.status(404).json();
-        }
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-});
 
 // Create Product
+const validationConfigCreateObj = [
+    { key: "name", type: "string", isRequired: true },
+    { key: "price", type: "number", isRequired: true }
+ ];
 router.post('/', async (req, res, next) => {
     
     try{
-        const {_id, name,description, price,timestamp } = req.body;
-            const validationConfig = ["name","description", "price","timestamp"];
+       
+        const isError = validateRequestPayload(req.body, validationConfigCreateObj)
             if(!isError.length){
-            const newProduct = new Product({"_id":_id,"name":name, "description":description, "price":price, "timestamp":timestamp});
-            await newProduct.save()
+                const newProduct = new Product(req.body)
+                await newProduct.save()
             // TODO : if product already exsist in db should return 409 
             return res.status(200).json(newProduct)
         }else{
@@ -87,6 +58,41 @@ router.post('/', async (req, res, next) => {
     }
 
 });
+
+
+// Product Update by Id 
+const validationConfigUpdateObj = [
+    { key: "name", type: "string", isRequired: false },
+    { key: "price", type: "number", isRequired: false },
+    { key: "desc", type: "string", isRequired: false }
+ ];
+router.put('/:id', async (req, res, next) => {
+    
+    // req.params eg : /products/:id = /products/1234 ->    id = req.params.id     =>  id = 1234
+    // req.query  eg : /products?page=1 ->                  page = req.query.page  => page = 1
+    // req.body   eg : { name : 'abc'} ->                   name = req.body.name   => name = "abc"
+
+    try{
+        const id = req.params.id
+        const isError = validateRequestPayload(req.body, validationConfigUpdateObj)
+        if(!isError.length){
+            const product = await Product.findOne({_id : id }).exec() 
+            if(product){                
+                const updateProduct = await Product.findOneAndUpdate({ _id: id }, req.body, { new: true } )
+                // TODO : if product already exsist in db should return 409 
+                return res.status(200).json(updateProduct)
+            }else{
+                return res.status(404).json()
+            }
+        }else{
+            return res.status(400).json(isError)
+        }
+    }catch(err){
+        return res.status(500).json()
+    }
+
+});
+
 
 // Delete Product by Id
 router.delete('/:id', async (req, res, next) => {
